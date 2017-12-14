7f3ce23d169aa617b81e46716e9177b93d7b5bc0
Hao Le K <hao.le2@asiantech.vn>
2017-12-14 10:08:05 +0700

    check last pager

--- app/src/main/java/com/kimhao/company/MainActivity.java
+++ app/src/main/java/com/kimhao/company/MainActivity.java
@@ -7,11 +7,16 @@ import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.TextView;
 
+import com.rd.PageIndicatorView;
+import com.rd.draw.data.Orientation;
+
 public class MainActivity extends AppCompatActivity {
 
     private String[] mStrings = {"Tab 1", "Tab 2", "Tab 3"};
     private ViewPager mViewPager;
     private TextView mTvSkip;
+    private PageIndicatorView mPageIndicatorView;
+    private Boolean mIsLastViewPager = false;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,30 +25,36 @@ public class MainActivity extends AppCompatActivity {
 
         mViewPager = findViewById(R.id.viewpager);
         mTvSkip = findViewById(R.id.tvSkip);
+        mPageIndicatorView = findViewById(R.id.pageIndicator);
+
         PagerAdapter pagerAdapter = new PagerAdapter(MainActivity.this, mStrings);
         mViewPager.setAdapter(pagerAdapter);
 
         mViewPager.addOnPageChangeListener(new ViewPager.OnPageChangeListener() {
             @Override
             public void onPageScrolled(int position, float positionOffset, int positionOffsetPixels) {
-
             }
 
             @Override
             public void onPageSelected(int position) {
                 // last viewpager
-                if (position == mStrings.length - 1) {
-                    mTvSkip.setVisibility(View.VISIBLE);
-                } else {
-                    mTvSkip.setVisibility(View.GONE);
-                }
+                mIsLastViewPager = position == mStrings.length - 1;
             }
 
             @Override
             public void onPageScrollStateChanged(int state) {
-
+                // Check if it last viewpager and scroll
+                if (mIsLastViewPager && state == ViewPager.SCROLL_STATE_DRAGGING) {
+                    startActivity(new Intent(MainActivity.this, SecondActivity.class));
+                    mIsLastViewPager = false;
+                }
             }
         });
+
+        mPageIndicatorView.setViewPager(mViewPager);
+        mPageIndicatorView.setCount(mStrings.length);
+        mPageIndicatorView.setOrientation(Orientation.HORIZONTAL);
+        mPageIndicatorView.setDynamicCount(true);
 
         mTvSkip.setOnClickListener(new View.OnClickListener() {
             @Override
